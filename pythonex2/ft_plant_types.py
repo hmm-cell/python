a5da604363f40420cc123932d8c1cee3fd080ae4
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-07-02 20:01:23 +0100

    Implement Plant, Flower, Tree, and Vegetable classes

---
+++ pythonex2/ft_plant_types.py
@@ -0,0 +1,69 @@
+class Plant:
+    def __init__(self, name="", height=0, age=0):
+        self.name = name
+        self.height = height
+        self.age = age
+
+class Flower(Plant):
+    def __init__(self, name, height, age, color)
+        super().__init__(name, height, age)
+        self.color = color
+        self.is_blooming = False
+
+    def bloom(self):
+        self.is_blooming = True
+        print(f"[asking the {self.name.lower()} to bloom]")
+        
+
+    def show(self):
+        print(f"=== Flower")
+        print(f"{self.name}: {round(self.height, 1)}cm, {self.age} days old.")
+        if self.is_blooming:
+            print(f"{self.name} is blooming beautifully!")
+        else:
+            print(f"{self.name} has not bloomed yet")
+
+class Tree(Plant):
+    def __init__(self, name, height, age,  trunk_diameter)
+        super().__init__(name, height, age)
+        self.trunk_diameter = trunk_diameter
+        self.shade_length = 0.0
+        self.shade_width = 0.0
+
+    def produce_shade(self):
+        self.shade_length = round(self.height, 1)
+        self.shade_width = round(self.trunk_diameter, 1)
+        print(f"Tree {self.name} now produces a shade of {self.shade_lenght} cm long and {self.shade_width} cm wide.")
+
+    def show(self):
+        print(f"=== Tree")
+        print(f"{self.name}: {round(self.height, 1)}cm, {self.age} days old.")
+        print(f"{round(self.trunk_diameter, 1)}cm")
+
+class Vegetable(Plant):
+    def __init__(self, name, height, age, harvest):
+        super().__init__(name, height, age)
+        self.harvest = harvest
+        self.nutrition = 0
+
+    def grow(self):
+        print(f"[make {self.name.lower()} grow and age for 20 days]")
+        self.nutrition = self.nutrition + 20
+        self.age = self.age + 20
+        self.height = self.height + 42.0
+        
+    def show(self):
+        print(f"=== Vegetable")
+        print(f"{self.name}: {round(self.height, 1)}cm, {self.age} days old.")
+        print(f"Harvest season: {self.harvest}")
+        print(f"Nutritional value: {self.nutrition}")
+
+tomato = Vegetable("Tomato", 5.0, 10, "April")
+tomato.show()
+print()
+tomato.grow()
+print()
+tomato.show()
+
+
+  
