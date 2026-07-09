152a2c0385f77544b3156f93510348487de7a69c
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-07-09 17:36:50 +0100

    Update ft_raise_exception.py

--- pythonex3/ft_raise_exception.py
+++ pythonex3/ft_raise_exception.py
@@ -15,12 +15,19 @@ def test_temperature():
         print(f"Input data is '{valid_res}'")
     except ValueError as err:
         print(f"Caught input_temperature error: {err}")
+                             
     print("Input data is 'abc'")
     try:
         valid_res = input_temperature("abc")
         print(f"Input data is '{valid_res}'")
     except ValueError as err:
         print(f"Caught input_temperature error: {err}")
+
+    print("Input data is '100'")
+    try:
+        valid_res = input_temperature("100")
+        print(f"Input data is '{}'")
     print("All tests completed - program didn't crash!")
+                             
 
 test_temperature()
