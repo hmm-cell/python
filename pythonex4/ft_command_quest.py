be26beee55ac9eb9aef3736878ac1f0433bab152
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-07-16 13:04:56 +0100

    Refactor argument handling in ft_command_quest.py

--- pythonex4/ft_command_quest.py
+++ pythonex4/ft_command_quest.py
@@ -2,12 +2,14 @@ import sys
 
 print("=== Command Quest ===")
 print(f"Program name: {sys.argv[0]}")
-print(f"Arguments recieved: {len(sys.argv) - 1}")
-
+if len(sys.argv) > 1:
+    print(f"Arguments received: {len(sys.argv) - 1}")
+else:
+    print(f"No arguments provided!")
 ctr = 1
 
 for arg in sys.argv[1:]:
     print(f"Argument {ctr}: {arg}")
-    ctr += 1;
+    ctr += 1
 
 print(f"Total arguments: {len(sys.argv)}")
