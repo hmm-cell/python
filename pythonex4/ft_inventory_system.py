7fdc3fca0ab96be2e4e022c99f6a5bc54337793e
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-07-27 11:40:41 +0100

    Add basic inventory system with command line input
    Implement a basic inventory system that accepts command line arguments in the format 'item:quantity'. The program checks for errors in input and updates the inventory accordingly.

---
+++ pythonex4/ft_inventory_system.py
@@ -0,0 +1,31 @@
+import sys
+
+inventory = {}
+
+arguments = sys.argv[1:]
+
+print(f"=== Inventory System Analysis ===")
+
+for argument in arguments:
+    if ":" not in {argument}:
+        print(f"Error - invalid parameter '{argument}'")
+        continue
+
+    parts = argument.split(":")
+        
+    item = parts[0]
+    if item in invetory.keys:
+        printf(f"Redundant item '{item}' - discarding")
+    try:
+        amount = int(parts[1])
+        inventory.update({item:amount})
+    except ValueError:
+        print(f"Quantity error for '{parts[0]}' y': invalid literal for int() with base 10: {parts[1]}")
+
+print(f"Got inventory {inventory}")
+
+items_list = list(inventory.keys())
+print(f"tem list: ")
+
+total = sum(inventory.values)
+print(f"total")
