09b19b1e5dd5df8e25ad5cbfa5a37c5e2ffb0686
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-07-27 13:17:41 +0100

    Correct typos and syntax errors in inventory system
    Fixed various typos and syntax errors in the inventory system code.

--- pythonex4/ft_inventory_system.py
+++ pythonex4/ft_inventory_system.py
@@ -7,19 +7,21 @@ arguments = sys.argv[1:]
 print(f"=== Inventory System Analysis ===")
 
 for argument in arguments:
-    if ":" not in {argument}:
+    if ":" not in argument:
         print(f"Error - invalid parameter '{argument}'")
         continue
 
     parts = argument.split(":")
         
     item = parts[0]
-    if item in invetory.keys:
+    if item in inventory.keys:
         printf(f"Redundant item '{item}' - discarding")
+        continue
+        
     try:
         amount = int(parts[1])
         inventory.update({item:amount})
-    except ValueError:
+    except ValueError as err:
         print(f"Quantity error for '{parts[0]}' y': invalid literal for int() with base 10: {parts[1]}")
 
 print(f"Got inventory {inventory}")
@@ -27,8 +29,8 @@ print(f"Got inventory {inventory}")
 items_list = list(inventory.keys())
 print(f"tem list: ")
 
-total_values = sum(inventory.values)
-total_keys = len(inventory.keys)
+total_values = sum(inventory.values())
+total_keys = len(inventory.keys())
 print(f"Total quantity of the {total_keys} items: {total_values}")
 if total_values > 0:
     for item, quantity in inventory.items():
