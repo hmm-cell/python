8a7bff5e272a360130adcf1aef43aeb82e6843c9
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-08-04 13:58:38 +0100

    Refactor archive creation script for clarity and functionality

--- pythonex5/ft_archive_creation.py
+++ pythonex5/ft_archive_creation.py
@@ -6,34 +6,55 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 filename = sys.argv[1]
-print(f"=== Cyber Archives Recovery ===")
-print(f"Accessing file'{filename}'")
+
+print("=== Cyber Archives Recovery & Preservation ===")
+print(f"Accessing file '{filename}'")
 
 try:
     file: typing.IO = open(filename, 'r')
     content = file.read()
-    print(f"---\n")
-    print(f"{content}")
-    print(f"---\n")
-    print(f"File '{filename}' closed.")
     file.close()
+    
+    print("---")
+    print(content, end="" if content.endswith("\n") else "\n")
+    print("---")
+    print(f"File '{filename}' closed.")
+    
 except Exception as err:
-    print(f"Error opening file '{nome_ficheiro}': {err}")
+    print(f"Error opening file '{filename}': {err}")
     sys.exit(1)
 
+modified_content = ""
 if content:
-    lines = content.split('\n')
-    modified_lines = ""
+    content_clean = content[:-1] if content.endswith('\n') else content
+    lines = content_clean.split('\n')
+    
     for line in lines:
-        modified_lines += line + "#\n"
+        modified_content += line + "#\n"
+        
+    if not content.endswith('\n'):
+        modified_content = modified_content[:-1]
 
-    modified_lines = modified_lines[:-1]
+print("Transform data:")
+print("---")
+print(modified_content, end="" if modified_content.endswith("\n") else "\n")
+print("---")
 
-    file = open(filename, 'w')
-    file.write(modified_lines)
-    print(f"---\n")
-    print(f"{modified_lines}")
-    print(f"---\n")
+new_filename = input("Enter new file name (or empty): ")
+
+if new_filename == "":
+    new_filename = filename
+
+print(f"Saving data to '{new_filename}'")
+
+try:
+    file = open(new_filename, 'w')
+    file.write(modified_content)
     file.close()
+    print(f"Data saved in file '{new_filename}'.")
+    
+except Exception as err:
+    print(f"Error saving file '{new_filename}': {err}")
+    sys.exit(1)
 
     
