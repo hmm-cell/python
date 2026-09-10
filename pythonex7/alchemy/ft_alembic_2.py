1281816f079de5acfdcc390546d3fbda9f6f153b
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-09-10 19:18:15 +0100

    Delete pythonex7/alchemy/ft_alembic_2.py

--- pythonex7/alchemy/ft_alembic_2.py
+++
@@ -1,9 +0,0 @@
-import alchemy.elements.create_earth()
-
-def main() -> None:
-    print("=== Alembic 0 ===")
-    print("Using: 'import ...' structure to access elements.py")
-    print(f"Testing create_water: {create_earth()}\n")
-
-if __name__ = "__main__"
-    main()
