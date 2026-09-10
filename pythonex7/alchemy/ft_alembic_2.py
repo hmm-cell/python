8a797f3f9bcc01ae62474253850ea570a56071da
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-09-10 19:08:41 +0100

    Add main function to ft_alembic_2.py
    Implement main function to test create_earth.

---
+++ pythonex7/alchemy/ft_alembic_2.py
@@ -0,0 +1,9 @@
+import alchemy.elements.create_earth()
+
+def main() -> None:
+    print("=== Alembic 0 ===")
+    print("Using: 'import ...' structure to access elements.py")
+    print(f"Testing create_water: {create_earth()}\n")
+
+if __name__ = "__main__"
+    main()
