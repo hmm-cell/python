2a5946c9caa8d23d3a10d9057303b84bc5e55067
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-09-11 17:42:56 +0100

    Add potions.py with healing and strength potion functions

---
+++ pythonex7/alchemy/potions.py
@@ -0,0 +1,10 @@
+from .elements import create_fire, create_water
+from elements import create_earth, create_air
+
+def healing_potion() -> str:
+    return("Healing potion brewed with"
+           f"'{create_earth()}' and '{create_air()}'")
+
+def strenght_potion() -> str:
+    return("Strenght potion brewed with"
+          f"'{create_earth()}' and '{create_air()}'")
