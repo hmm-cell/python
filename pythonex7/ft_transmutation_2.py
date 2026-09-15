2bc5cf92b9a48fcc58fe9a4778064b8c8029c135
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-09-15 19:40:28 +0100

    Create ft_transmutation_2.py

---
+++ pythonex7/ft_transmutation_2.py
@@ -0,0 +1,10 @@
+import alchemy
+
+def main() -> None:
+    print("=== Transmutation 2 ===")
+    print("Import alchemy module only")
+    print("Testing lead to gold: "
+          f"{alchemy.lead_to_gold()}")
+
+if __name__ == "__main__":
+    main()
